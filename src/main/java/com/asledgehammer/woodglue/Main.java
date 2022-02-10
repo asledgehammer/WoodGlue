22bfb5fcd4c60dc3c02a3b1d0c472137f0267003
JabDoesThings <JabDoesThings@github.com>
2022-02-09 22:45:17 -0600

    Initial commit.

---
+++ src/main/java/com/asledgehammer/woodglue/Main.java
@@ -0,0 +1,18 @@
+package com.asledgehammer.woodglue;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+  public static void main(String[] args) {
+    try {
+      Class.forName("zombie.network.GameServer")
+          .getDeclaredMethod("main", String[].class)
+          .invoke(null, (Object[]) args);
+    } catch (IllegalAccessException
+        | InvocationTargetException
+        | NoSuchMethodException
+        | ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+}
